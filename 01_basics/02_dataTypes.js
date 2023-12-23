4d0cf182b0b29e8f98796da8a475b274975baa62
Abdul Razzaq <a.razzaq4085@gmail.com>
2023-12-23 23:44:39 +0500

    02 datatypes completed

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello"); // we are using node js not browser
+
+console.log(3 + 3);  
+
+// code readability should be high
+
+
+let name = "Abdul"
+let age = 18
+let isLoggedIn = false
+
+
+// Primitives Datatypes
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// null is 
+
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) //object
+
+
+
