d5a1089d98f182944cb113cdc50786d44cdf19e7
Abdul Razzaq <a.razzaq4085@gmail.com>
2023-12-24 00:17:08 +0500

    Operations covnersion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,13 +1,13 @@
 let score = "33abc3"
 
 
-console.log(typeof score)
+// console.log(typeof score)
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber)
+// console.log(typeof valueInNumber)
 
 // "33" => 33
 // "33abc" => NaN but type number
@@ -17,7 +17,7 @@ console.log(typeof valueInNumber)
 let isLoggedIn = ""
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true; 0 => false
 //  "" => false
@@ -26,4 +26,50 @@ console.log(booleanIsLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
+// console.log(typeof stringNumber)
+
+
+// ************* Operations *****************
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 ** 3)
+// console.log(2 / 3)
+// console.log(2 % 3)
+
+let str = "hello"
+let str2 = " Dear"
+let str3 = str + str2
+
+// console.log(str3)
+
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(2 + 2 + "2")
+
+
+console.log(true)
+
+console.log(+"")
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+console.log(num1, num2, num3)
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
+
+
+// prefix and postfix
+
