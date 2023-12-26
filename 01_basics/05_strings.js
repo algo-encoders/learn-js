d0e675b16b3244ec1a1f3e2901ecd2832cc30a39
Abdul Razzaq <a.razzaq4085@gmail.com>
2023-12-26 16:39:46 +0500

    Strings clear

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,29 @@
+const name = "abdul"
+const repoCount = 32
+
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Hellosdfewr");
+
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length)
+
+// console.log(gameName.toUpperCase())
+
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf("h"))
+
+// console.log(gameName.split('l'))
+
+// console.log(gameName.substring(1, 3))
+
+const anotherString = gameName.slice(4, 6);
+const anotherString2 = gameName.slice(-4, -1);
+const anotherString23 = gameName.slice(-4);
+console.log(gameName.trimEnd())
+const url = "https://hello.com?test=a%20"
+gameName.includes("hello")
+
+console.log("I am other::", anotherString);
