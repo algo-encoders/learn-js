e70b7170b66751c0c8e8deb2595c4df0ec935328
Abdul Razzaq <a.razzaq4085@gmail.com>
2023-12-26 15:45:41 +0500

    Heap & Stack

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -14,15 +14,15 @@ const id = Symbol('123')
 const anotherId = Symbol('123')
 
 
-console.log(id)
-console.log(id === anotherId)
+// console.log(id)
+// console.log(id === anotherId)
 // Reference (Non Primitives)
 
 // Array, Objects, Functions
 
 const bigNumber = 2323432432432432432432222222n
 
-console.log(typeof bigNumber)
+// console.log(typeof bigNumber)
 
 // cannot perform math. on bigint and int both should be bigint
 
@@ -36,7 +36,30 @@ const myFunction = function(){
     console.log("hello world");
 }
 
-console.log(typeof heros)
-console.log(typeof myFunction)
-console.log(typeof myObj)
+// console.log(typeof heros)
+// console.log(typeof myFunction)
+// console.log(typeof myObj)
 
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutube = "AbdulRazzaq"
+let anothername = myYoutube
+anothername = "chaiorcode"
+
+console.log(myYoutube)
+console.log(anothername)
+
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+console.log(userTwo)
+userTwo.email = "xyz@test.com"
+console.log(userTwo)
